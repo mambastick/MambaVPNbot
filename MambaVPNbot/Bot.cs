bacf3bdd81c90eea32dfd8189fb74283b9bf30df
mamba <neelov.ev@mail.ru>
2023-12-21 16:56:40 +0300

    Added Bot.cs for creating bot

--- MambaVPNbot/Bot.cs
+++ MambaVPNbot/Bot.cs
@@ -1,8 +1,52 @@
+using MambaVPNbot.Handlers;
+using MambaVPNbot.Handlers.General;
 using MLoggerService;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
 
 namespace MambaVPNbot;
 
-public class Bot(string token, MLogger logger)
+public class Bot
 {
+    public Bot(string token, MLogger logger, string botPassword = "MambaVPN")
+    {
+        Logger = logger;
+        Logger.LogProcess("Initializing bot...");
+
+        BotClient = new TelegramBotClient(token);
+        BotPassword = botPassword;
+        
+        Logger.LogSuccess("Bot has been initialized.");
+    }
+
+    public static string BotPassword { get; set; }
+    public static MLogger Logger { get; private set; }
+    private ITelegramBotClient BotClient { get; }
     
+    public Task StartAsync()
+    {
+        Logger.LogProcess("Start receive updates...");
+        
+        using var cts = new CancellationTokenSource();
+
+        var receiverOptions = new ReceiverOptions()
+        {
+            ThrowPendingUpdates = true,
+            AllowedUpdates =
+            [
+                UpdateType.Message,
+                UpdateType.CallbackQuery,
+            ]
+        };
+        
+        _ = BotClient.ReceiveAsync(
+            new UpdateHandler().GetUpdatesAsync,
+            new ErrorHandler().GetApiError,
+            receiverOptions, 
+            cancellationToken: cts.Token);
+        
+        Logger.LogSuccess("Bot receiving updates.");
+        return Task.CompletedTask;
+    }
 }
